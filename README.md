caef458c2e49db8680827ad6a07c8ee4e4681348
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-04-11 12:58:51 -0500

    Changed youtube api so we have more controll over the video

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Hack_Sprint
 Repo for the hack sprint.
+
+
+For presentation:
+  Tech used: HTML5, CSS, Javascript, Jquery, Bootstrap, Youtube API
--- public/static/scripts/index.js
+++ public/static/scripts/index.js
@@ -1,9 +1,42 @@
+// This is setting up the youtube video player and giving us
+// a method to play and pause the video when an event happens.
+// Call player.playVideo() to play the video and player.pauseVideo() to pause the video.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'tgbNymZ7vqY',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+  secondsToPlay = 20;
+  if (event.data == YT.PlayerState.PLAYING) {
+    setTimeout(stopVideo, (secondsToPlay * 1000));
+  }
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
+
 $(document).ready(function () {
   // Add click event listener to the hamburger button
   $(".header_hamburger").on('click', function () {
     // Toggle the sidebar-open and sidebar-closed classes
     $(".sidebar").toggleClass('sidebar-closed');
   });
+
+  // SIGNING button click event
   $('.signin button').on('click', function () {
     var username = $('#username').val();
     var password = $('#password').val();
@@ -14,6 +47,15 @@ $(document).ready(function () {
       alert('Correct!');
     } else {
       alert('Incorrect. Please try again.');
+    }
+  });
+
+  // YOUTUBE video
+  $('#player').on('click', function () {
+    if (player.getPlayerState() === YT.PlayerState.PLAYING) {
+      player.pauseVideo();
+    } else {
+      player.playVideo();
     }
   });
 });
