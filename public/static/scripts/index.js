a6e68ca33a7b63f6f4c5d81c317eb7181201c51a
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-04-11 11:53:03 -0500

    Refactor footer layout, add center alignment to footer.css, and add hamburger button functionality to index.js

--- public/static/scripts/index.js
+++ public/static/scripts/index.js
@@ -1,5 +1,19 @@
-// $(document).ready(function () {
-//   $(".header_contact_text, .header_about_text").click(function () {
-//     $(this).next().toggle();
-//   });
-// });
+$(document).ready(function () {
+  // Add click event listener to the hamburger button
+  $(".header_hamburger").on('click', function () {
+    // Toggle the sidebar-open and sidebar-closed classes
+    $(".sidebar").toggleClass('sidebar-closed');
+  });
+  $('.signin button').on('click', function () {
+    var username = $('#username').val();
+    var password = $('#password').val();
+    var correctUsername = 'admin';
+    var correctPassword = 'admin';
+    console.log("Button clicked")
+    if (username === correctUsername && password === correctPassword) {
+      alert('Correct!');
+    } else {
+      alert('Incorrect. Please try again.');
+    }
+  });
+});
